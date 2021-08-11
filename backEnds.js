44846774a38e8c1fa3aaa8c9f39a04790ed95620
Jorel Martins <bearshamis@gmail.com>
2021-08-11 11:57:11 -0300

    fazendo

---
+++ backEnds.js
@@ -0,0 +1,23 @@
+const entradaDoSUl = 30;
+const SuperZottis = 400;
+
+const ICMS = 18;
+const IPI = 4;
+const PIS = 1.86;
+const COFINS= 8.54;
+
+alert("Digite o quanto voce quer de energeticos :")
+prompt("Escolha valor",alert)
+console.log(prompt)
+
+function supermercadoDoSul(){
+  let energetico = 4.50;
+
+  
+  
+}
+
+function SuperZottis(){
+  
+
+}
